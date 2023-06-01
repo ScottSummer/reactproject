ccb0ba3e9b840958674afc8c90c74116b8fdf302
ScottSummer <87394222+ScottSummer@users.noreply.github.com>
2023-06-01 22:29:02 +0300

    react-tutorial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{ Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class ExampleComponent extends Component{
+  
+  constructor(props) {
+    super(props)
+    console.log('Constructor Çalıştı')
+    this.state = {
+      message: 'Merhaba Dünya'
+    }
+  }
+
+  componentDidMount() {
+    console.log('ComponentDidMount çalıştı')
+  }
+
+
+  componentDidUpdate() {
+    console.log('ComponentDidUpdate çalıştı')
+  }
+
+  componentWillUnmount() {
+    console.log('ComponentWillUnmount çalıştı')
+  }
+
+  render() {
+    console.log('çalıştı')
+    return(
+
+      <div>
+        <h1>{this.state.message}</h1>
+        <button onClick={() => this.setState({message: 'Güle Güle Dünya'})}>Güncelle</button>
+      </div>
+
+    )
+  }
 }
 
-export default App;
+
+
+export default ExampleComponent;
